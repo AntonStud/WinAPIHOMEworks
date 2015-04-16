49217be6c4f797573afd73da2db2148ec3e8bf7c
AntonStud <toher2008@gmail.com>
2015-04-16 16:47:31 +0300

    JavaScript3

---
+++ WinAPIHomework/JavaScript1_19.04.2015/js/HomeWork17.04.2015.js
@@ -0,0 +1,11 @@
+/**
+ * Created by User on 16.04.2015.
+ */
+
+
+
+// Получаем элемент с id AddButton
+var button = document.getElementById("AddButton");
+
+// Привязываем обработчик нажатия на кнопку
+button.addEventListener("click", SearchText);
